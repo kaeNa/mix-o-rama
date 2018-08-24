bbda7085a7394a4af84cee0c5a51d6d2b59b6065
Aleksandr Bogdanov <syn@syn.im>
2018-08-24 23:18:02 +0200

    Finally sorted out the pin situation

--- main.py
+++ main.py
@@ -2,75 +2,54 @@ import sys
 import time
 from RPi import GPIO
 import logging
-
-logging.basicConfig(stream=sys.stdout, level=logging.INFO)
-logger = logging.getLogger(__name__)
-
 from mixorama.bartender import Bartender
 from mixorama.io import Button, Valve
 from mixorama.recipes import Component, GIN_TONIC, CUBA_LIBRE
 from mixorama.scales import Scales
 
-#                               pin #3 is usable as output     which is BCM 2
-#                               pin #5 is usable as output     which is BCM 3
-# pin #7 is usable as input     pin #7 is usable as output     which is BCM 4
-# pin #8 is usable as input     pin #8 is usable as output     which is BCM 14
-# pin #10 is usable as input    pin #10 is usable as output    which is BCM 15
-# pin #11 is usable as input    pin #11 is usable as output    which is BCM 17
-
-# pin #12 is usable as input    pin #12 is usable as output    which is BCM 18
-# pin #13 is usable as input    pin #13 is usable as output    which is BCM 27
-# pin #15 is usable as input    pin #15 is usable as output    which is BCM 22
-# pin #16 is usable as input    pin #16 is usable as output    which is BCM 23
-# pin #18 is usable as input    pin #18 is usable as output    which is BCM 24
-# pin #19 is usable as input    pin #19 is usable as output    which is BCM 10
-# pin #21 is usable as input    pin #21 is usable as output    which is BCM 9
-# pin #22 is usable as input    pin #22 is usable as output    which is BCM 25
-# pin #23 is usable as input    pin #23 is usable as output    which is BCM 11
-# pin #24 is usable as input    pin #24 is usable as output    which is BCM 8
-# pin #26 is usable as input    pin #26 is usable as output    which is BCM 7
-# pin #29 is usable as input    pin #29 is usable as output    which is BCM 5
-# pin #35 is usable as input                                   which is BCM 19
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+logger = logging.getLogger('mixorama')
 
 GPIO.setmode(GPIO.BCM)
 
-components = {
-    Component.GIN: Valve(18),  # 12
-    Component.TONIC: Valve(27),  # 13
-    # Component.RUM: Valve(22),  # 15
-    # Component.COLA: Valve(23),  # 16
-    # Component.TEQUILA: Valve(24),  # 18
-    # Component.COINTREAU: Valve(10),  # 19
-}
+scales = Scales(4, 3)  # dout=7, sck=5
 
-scales = Scales(9, 25)  # pins 21, 22
+components = {
+    Component.GIN: Valve(5),  # 29
+    Component.TONIC: Valve(6),  # 31
+    Component.RUM: Valve(13),  # 33
+    Component.COLA: Valve(19),  # 35
+    Component.TEQUILA: Valve(26),  # 37
+    Component.COINTREAU: Valve(0),  # 39
+}
 
 bar = Bartender(components, scales)
 
 
 def request_drink(recipe):
     def ui():
-        logger.info('Making a {}'.format(recipe))
+        print('Making a {}'.format(recipe))
         if bar.make_drink(recipe):
-            logger.info('Your drink is ready! Please take it from the tray')
+            print('Your drink is ready! Please take it from the tray')
             bar.serve()
         else:
-            logger.info('Could not make a drink')
+            print('Could not make a drink')
     return ui
 
 
-# abort_btn = Button(11, lambda: bar.abort() and print('Please discard the glass contents'))  # 23
-# gin_tonic_btn = Button(8, request_drink(GIN_TONIC))  # 24
-# cuba_libre_btn = Button(7, request_drink(CUBA_LIBRE))  # 26
+Button(12, lambda: bar.abort() and print('Please discard the glass contents'))  # 32
 
-# Button(9, lambda: print('not assigned'))  # 21
-# Button(19, lambda: print('not assigned'))  # 35
-# Button(15, lambda: print('not assigned'))  # 10
-# Button(17, lambda: print('not assigned'))  # 11
+Button(23, request_drink(GIN_TONIC))  # 16
+Button(24, request_drink(CUBA_LIBRE))  # 18
+Button(25, lambda: print('not assigned'))  # 22
+Button(8, lambda: print('not assigned'))  # 24
+Button(7, lambda: print('not assigned'))  # 26
+Button(1, lambda: print('not assigned'))  # 28
 
 
 logger.info('Ready to make cocktails!')
-
-
-while True:
-    time.sleep(5)
+try:
+    while True:
+        time.sleep(5)
+finally:
+    GPIO.cleanup()
--- mixorama/io.py
+++ mixorama/io.py
@@ -1,13 +1,16 @@
+import logging
 import warnings
 from RPi import GPIO
 
+logger = logging.getLogger(__name__)
 
-class Button():
+
+class Button:
     def __init__(self, channel, callback=lambda: None):
         with warnings.catch_warnings(record=True) as w:
             GPIO.setup(channel, GPIO.IN, pull_up_down=GPIO.PUD_UP)
             if len(w) > 0:
-                print('{} ...when trying to setup button on channel {}'.format(w[0].message, channel))
+                logger.warn('{} ...when trying to setup button on channel {}'.format(w[0].message, channel))
 
         GPIO.add_event_detect(channel, GPIO.FALLING, callback=self._callback, bouncetime=400)
         self.cb = callback
@@ -16,13 +19,13 @@ class Button():
         self.cb()
 
 
-class Valve():
+class Valve:
     def __init__(self, channel):
         self.channel = channel
         with warnings.catch_warnings(record=True) as w:
             GPIO.setup(self.channel, GPIO.OUT)
             if len(w) > 0:
-                print('{} ...when trying to setup valve on channel {}'.format(w[0].message, self.channel))
+                logger.warn('{} ...when trying to setup valve on channel {}'.format(w[0].message, self.channel))
 
     def open(self):
         GPIO.output(self.channel, 1)
